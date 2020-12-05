7d5926139e72f0ca8854c6e54045c810249ed9e9
Zak Ali <zak.ali@thetin.net>
2020-12-05 17:15:40 +0000

    Add route setup and a palette specific route

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,23 @@ import React from "react"
 import seedsPalette from "../seedPalettes"
 import Palette from "./Palette"
 import {generatePalette} from "../generatePalette"
+import {Switch, Route} from "react-router-dom"
 
 function App() {
+  const findPalette = (id) => {
+    return seedsPalette.find((palette) => (
+      palette.id === id
+    ))
+  }
   return (
     <div className="App">
-      <Palette palette={generatePalette(seedsPalette[4])} />
+      <Switch>
+        <Route exact path="/" render={() => <h1>Homepage</h1>} />
+        <Route exact path="/palette/:id" render={(routeProps) => {
+          console.log(routeProps)
+          return <Palette palette={generatePalette(findPalette(routeProps.match.params.id))} />
+        }}/>
+      </Switch>
     </div>
   );
 }
